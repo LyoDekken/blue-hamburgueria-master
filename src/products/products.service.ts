dcabb3b80bc89467007bc41d6d459ea7ae611f1e
LyoDekken <humberto.araripe12@gmail.com>
2022-07-06 07:15:31 -0300

    fix: module name patterns

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -16,15 +16,15 @@ export class ProductsService {
     return this.prisma.product.findMany();
   }
 
-  findOne(id: string) {
+  findOne(id: string): Promise<Product> {
     return this.prisma.product.findUnique({ where: { id } });
   }
 
-  update(id: string, dto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, dto: UpdateProductDto): Promise<Product> {
+    return this.prisma.product.update({ where: { id }, data: dto });
   }
 
   remove(id: string) {
-    return `This action removes a #${id} product`;
+    return this.prisma.product.delete({ where: { id } });
   }
 }
