f0c4e2d6692852b41787ef5635999ada34db997f
LyoDekken <humberto.araripe12@gmail.com>
2022-07-06 07:01:14 -0300

    feature: configs of products resource

---
+++ src/products/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateProductDto): Promise<Product> {
+    return this.prisma.product.create({ data: dto });
+  }
+
+  findAll(): Promise<Product[]> {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.product.findUnique({ where: { id } });
+  }
+
+  update(id: string, dto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} product`;
+  }
+}
